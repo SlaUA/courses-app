1e7b98b2e5c866c28fb01b66423bb9a003a0f6a3
SlaUA <guitarman@i.ua>
2018-06-18 23:29:27 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CoursesPageComponent } from './courses-page/courses-page.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoursesPageComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
