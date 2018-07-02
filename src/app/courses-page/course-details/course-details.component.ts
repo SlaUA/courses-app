2a753974e240a9a155da900b06b7c0c118bdc9ea
Lubenets Slava <guitarman@i.ua>
2018-07-02 16:41:26 +0300

    add tests

--- src/app/courses-page/course-details/course-details.component.ts
+++ src/app/courses-page/course-details/course-details.component.ts
@@ -5,10 +5,12 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.css']
 })
-export class CourseDetailsComponent {
+export class CourseDetailsComponent implements OnInit {
   @Input() data: object;
   @Output() deleteCourse = new EventEmitter<number>();
   @Output() editCourse = new EventEmitter<number>();
+
+  ngOnInit() {}
 
   onDeleteCourse(courseId: number) {
     this.deleteCourse.emit(courseId);
--- src/app/courses-page/courses-page.component.spec.ts
+++ src/app/courses-page/courses-page.component.spec.ts
@@ -3,6 +3,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { CoursesPageComponent } from './courses-page.component';
+import { text } from '@angular/core/src/render3/instructions';
 
 describe('CoursesPageComponent', () => {
   let component: CoursesPageComponent;
@@ -24,5 +25,28 @@ describe('CoursesPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call console.log with input value when clicking on find button', () => {
+    spyOn(console, 'log');
+    const textToFind = 'course # 1';
+    component.findInputValue = textToFind;
+    component.findCourse();
+    expect(console.log).toHaveBeenCalledWith(`text to find: ${textToFind}`);
+  });
+  it('should NOT call console.log when clicking on find button with empty input', () => {
+    spyOn(console, 'log');
+    component.findCourse();
+    expect(console.log).not.toHaveBeenCalled();
+  });
+  it('should clear input value after clicking on find button', () => {
+    const textToFind = 'course # 1';
+    component.findInputValue = textToFind;
+    component.findCourse();
+    expect(component.findInputValue).toEqual('');
+  });
+  it('should call console.log after clicking on load more button', () => {
+    const spy = spyOn(console, 'log');
+    component.loadMoreCourses();
+    expect(spy).toHaveBeenCalledWith('loading more...');
   });
 });
