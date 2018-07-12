a8782d0c232705b65ce402c4ea70246fdda3cc86
Lubenets Slava <guitarman@i.ua>
2018-07-12 09:53:34 +0300

    use ngClass to change star color

--- src/app/core/pipes/order-by/order-by.pipe.ts
+++ src/app/core/pipes/order-by/order-by.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Course } from '../../services/courses-service/courses.service';
 
 @Pipe({
-  name: 'orderBy'
+  name: 'orderBy',
+  pure: false
 })
 export class OrderByPipe implements PipeTransform {
   transform(value: Course[]): any {
