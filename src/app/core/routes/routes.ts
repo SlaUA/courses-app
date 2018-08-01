215504036e6dd2caffc2dca491697da91426e7cf
Lubenets Slava <guitarman@i.ua>
2018-08-01 22:52:49 +0300

    add 404, edit course page, routing

--- src/app/core/routes/routes.ts
+++ src/app/core/routes/routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { CoursesPageComponent } from '../../courses-page/courses-page.component';
 import { LoginPageComponent } from '../../login-page/login-page.component';
 import { AddCoursePageComponent } from '../../add-course-page/add-course-page.component';
+import { PageNotFoundComponent } from '../../page-not-found/page-not-found.component';
+import { EditCourseComponent } from '../../edit-course/edit-course.component';
 
 export const routes: Routes = [
   { path: '', component: CoursesPageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: 'newCourse', component: AddCoursePageComponent },
+  { path: '/courses/new', component: AddCoursePageComponent },
+  { path: '/courses/:id', component: EditCourseComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
