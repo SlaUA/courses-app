b1769fbc42c9d4b9e22a103e141f9c5450f7d88a
Lubenets Slava <guitarman@i.ua>
2018-08-02 11:00:03 +0300

    add guard service for routing

--- src/app/core/routes/routes.ts
+++ src/app/core/routes/routes.ts
@@ -4,12 +4,13 @@ import { LoginPageComponent } from '../../login-page/login-page.component';
 import { AddCoursePageComponent } from '../../add-course-page/add-course-page.component';
 import { PageNotFoundComponent } from '../../page-not-found/page-not-found.component';
 import { EditCourseComponent } from '../../edit-course/edit-course.component';
+import { AuthGuardService } from '../services/auth-guard/auth-guard.service';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'courses', pathMatch: 'full' },
-  { path: 'courses', component: CoursesPageComponent },
+  { path: 'courses', component: CoursesPageComponent, canActivate: [AuthGuardService]  },
   { path: 'login', component: LoginPageComponent },
-  { path: 'courses/new', component: AddCoursePageComponent },
-  { path: 'courses/:id', component: EditCourseComponent },
+  { path: 'courses/new', component: AddCoursePageComponent, canActivate: [AuthGuardService] },
+  { path: 'courses/:id', component: EditCourseComponent, canActivate: [AuthGuardService] },
   { path: '**', component: PageNotFoundComponent }
 ];
