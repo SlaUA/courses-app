10a3c6967d927d4f9c949cba8c0b4ebcae75a85a
Lubenets Slava <guitarman@i.ua>
2018-09-10 23:25:20 +0300

    add reactive forms when editing a course

--- src/app/edit-course/edit-course.component.ts
+++ src/app/edit-course/edit-course.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CoursesService, Course } from '../core/services/courses-service/courses.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { CoursesService } from '../core/services/courses-service/courses.service';
 
 @Component({
   selector: 'app-edit-course',
@@ -8,8 +10,26 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./edit-course.component.css']
 })
 export class EditCourseComponent implements OnInit {
-  currentCourse: Course | null = null;
   currentCourseId: number | null = null;
+  currentCourseForm = new FormGroup({
+    name: new FormControl('', Validators.compose([
+      Validators.required,
+      // Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    description: new FormControl('', Validators.compose([
+      Validators.required,
+      // Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    date: new FormControl('', Validators.compose([
+      Validators.required,
+      // Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    length: new FormControl('', Validators.compose([
+      Validators.required,
+      // Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    isTopRated: new FormControl(false),
+  });
 
   constructor(
     private coursesService: CoursesService,
@@ -21,13 +41,19 @@ export class EditCourseComponent implements OnInit {
     this.activatedRoute.params.subscribe((params) => {
       this.currentCourseId = parseInt(params.id, 10);
       this.coursesService.getCourseById(this.currentCourseId).subscribe((course)=>{
-        this.currentCourse = course;
+        if (course) {
+          this.currentCourseForm.controls.name.setValue(course.name);
+          this.currentCourseForm.controls.description.setValue(course.description);
+          this.currentCourseForm.controls.date.setValue(course.date);
+          this.currentCourseForm.controls.length.setValue(course.length);
+          this.currentCourseForm.controls.isTopRated.setValue(course.isTopRated);
+        }
       });
     });
   }
 
   onUpdateCourse() {
-    this.coursesService.updateCourse(this.currentCourseId, this.currentCourse);
+    this.coursesService.updateCourse(this.currentCourseId, this.currentCourseForm.value);
     this.router.navigateByUrl('courses');
   }
 
